17542f61eae74b82ccb15c1517bc0be20c9ccf28
Tony CHAN <chankwongyintony@gmail.com>
2019-06-04 04:06:48 +0800

    Update index.php

--- index.php
+++ index.php
@@ -8,13 +8,13 @@
        <?php include "./view/title.php";?>
         <!-- post container -->
         <div class="row">
-            <div class="col-3"></div>
-            <div class="col-6">
+            <div class="col-sm-3"></div>
+            <div class="col-sm-6">
                 <!-- posts -->
                 <?php include "./view/post.php";?>
 
             </div>
-            <div class="col-3"> </div>
+            <div class="col-sm-3"> </div>
 
         </div>
     </div>
